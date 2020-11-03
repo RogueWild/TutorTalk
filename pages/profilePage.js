652f80c22811f6689d8d3ac480004138078cc45e
= <=>
2020-11-02 22:22:50 -0800

    created profile page

---
+++ pages/profilePage.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import LogoHeader from '../comps/LogoHeader';
+import Nav from '../comps/Nav';
+import ProfilePic from '../comps/ProfilePic';
+import Summary from '../comps/Summary';
+import Contact from '../comps/Contact';
+import AboutBox from '../comps/About';
+
+export default function profilePage(){
+    return (
+        <div>
+        <LogoHeader />
+        <Nav />
+        <div className="content-box">
+            <div className="left-side">
+                <ProfilePic />
+                <div className="contact"><Contact /></div>
+            </div>
+            <div className="right-side">
+                <Summary />
+                <div className="about-box">
+                    <AboutBox color={0} text2={"About"} text={"I’m a Computer Science student at BCIT, and I’m passionate about helping others learn while I learn! I have a good understanding of interdisciplinary computer programming. Ask me anything and I’ll try to help you out. Email me or give me a call and leave a voice mail. I like to teach through Zoom; once you reach out and we seem like a good student/ tutor fit, I will set up a meeting for us on Zoom."} size={0} />
+                </div>
+                <div className="about-box">
+                    <AboutBox color={0} text2={"Tips"} text={"Make sure you take notes during class and complete your homework as soon as possible once it has been assigned. That way the lesson is still fresh in your mind."} size={0}/>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
