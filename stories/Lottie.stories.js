2bebc3392a6db1df366652b3045f4c8ad45e66bf
RogueWild <0.dreamer.009@gmail.com>
2020-11-10 15:43:03 +0300

    minor fix

--- stories/Lottie.stories.js
+++ stories/Lottie.stories.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import TutorSuccessLottie from '../comps/TutorSuccessLottie';
+import TutorSuccessLottie from '../comps/Lotties/TutorSuccessLottie';
+import ErrorLottie from '../comps/Lotties/ErrorLottie';
+import StudentSuccessLottie from '../comps/Lotties/StudentSuccessLottie';
 
 export default {
     title: 'Lotties/TutorSuccessLottie',
-    component: TutorSuccessLottie
+    component: TutorSuccessLottie, ErrorLottie, StudentSuccessLottie
 };
 
 export const BasicTutorSuccessLottie = () => <TutorSuccessLottie />;
+export const BasicErrorLottie = () => <ErrorLottie />;
+export const BasicStudentSuccessLottie = () => <StudentSuccessLottie />;
