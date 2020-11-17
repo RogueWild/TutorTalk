cc2f7656ed4785111c55de1182fa7df49a290696
Julia <julia@Julias-MacBook-Pro.local>
2020-11-17 10:34:41 -0800

    page changes

--- pages/AddTips.js
+++ pages/AddTips.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import HeaderAccount from '../comps/Header';
 import LogoHeader from '../comps/LogoHeader';
 import Nav from '../comps/Nav';
+import InputText from '../comps/Input';
+import Button from '../comps/Button';
 
 export default function AddTipsPage() {
-    return (
-      <div>
-          <LogoHeader />
-          <Nav />
-          <div>
-          </div>
+  return (
+    <div>
+      <LogoHeader />
+      <Nav />
+      <div className="TipsContent">
+        <HeaderAccount />
+        <br></br>
+        <InputText />
+      </div >
+      <div className="Button">
+      <Button />
       </div>
-    )
-  }
+    </div>
+  )
+}
--- pages/tips.js
+++ pages/tips.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '../comps/Button';
+import HeaderAccount from '../comps/Header';
 import LogoHeader from '../comps/LogoHeader';
 import Nav from '../comps/Nav';
 import Tips from '../comps/Tips';
@@ -6,18 +8,25 @@ import Tips from '../comps/Tips';
 export default function TipsPage() {
   return (
     <div>
-        <LogoHeader />
-        <Nav />
+      <LogoHeader />
+      <Nav />
+      <div className="TipsContent">
+      <br></br>
+        <HeaderAccount />
+      <div className="Button2">
+        <Button />
+      </div>
+      <div className="Tips">
         <br></br>
-        <div className="Tips">
-            <Tips />
-            <br></br>
-            <Tips />
-            <br></br>
-            <Tips />
-            <br></br>
-            <Tips />
-        </div>
+        <Tips />
+        <br></br>
+        <Tips />
+        <br></br>
+        <Tips />
+        <br></br>
+        <Tips />
+      </div>
     </div>
+  </div>
   )
 }
